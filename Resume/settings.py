a6319d21b65a548753af2418a41047b234638e13
Clinton Felix <archimedesfelix3@gmail.com>
2021-08-21 22:29:27 +0100

    first commit

--- Resume/settings.py
+++ Resume/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-inb)n!#!x^77=l=rh#rbn6@)20z*1##598!3_e91!3glt!+@!0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','clintonfelixapp.herokuapp.com']
+ALLOWED_HOSTS = ['clintonfelixapp.herokuapp.com']
 
 
 # Application definition
